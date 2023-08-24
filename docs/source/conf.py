90be5e1f9a90642315d7f3146a5c5623b125518a
zli00185 <zli00185@terpmail.umd.edu>
2023-08-24 20:25:23 +0800

    conf.py modify

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,12 +12,15 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
+    'myst_parser',
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
 ]
+
+source_suffix = ['.rst', '.md']
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
